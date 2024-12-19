1b82040e612db4e7ad3b4b30e7237dd4444545ae
javaidb <javaidbaksh@gmail.com>
2024-12-18 22:53:12 -0500

    Moved PDF looper to PDFinterpreter.

--- src/modules/pdf_interpreter.py
+++ src/modules/pdf_interpreter.py
@@ -1,9 +1,12 @@
 import pdfplumber
 import re
+import pandas as pd
+from tqdm.notebook import tqdm_notebook
 
+from src.modules.helper_fns import *
 
 def extract_features_from_pdf(pdf_file, x_tolerance=2, init_y_top=440, reg_y_top=210):
-    
+
     regular_page_box = (70, reg_y_top, 400, 730)
     initial_page_box = (70, init_y_top, 400, 730)
 
@@ -70,3 +73,21 @@ def process_transactions_from_lines(pdf_lines, account_name):
             transactions.append(transaction)
 
     return transactions
+
+def generate_fin_df():
+    overall_df = pd.DataFrame()
+    account_names = read_all_account_folder_names()
+    for account_name in ['Chequing', 'Credit']:
+        pdf_files = read_all_files(account_name)
+        for pdf_file in tqdm_notebook(pdf_files, desc="Reading PDFs"):
+            pdf_file_atts = grab_pdf_name_attributes(pdf_file)
+            pdf_file_path = process_import_path(pdf_file, account_name)
+            lines = extract_features_from_pdf(pdf_file_path, x_tolerance=2, init_y_top=440, reg_y_top=210)
+            transactions= process_transactions_from_lines(lines, account_name)
+            temp_df = pd.DataFrame(transactions)
+            temp_df[['Year']] = pdf_file_atts['year']
+            temp_df[['Account']] = account_name
+            overall_df = pd.concat([temp_df, overall_df], ignore_index=True)
+    overall_df['DateTime'] = overall_df['Transaction Date'] + ' ' + overall_df['Year'].astype(str)
+    overall_df['DateTime'] = pd.to_datetime(overall_df['DateTime'])
+    return overall_df
