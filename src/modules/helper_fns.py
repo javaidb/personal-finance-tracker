b8bc223196bee5d16bdb0cfc96a98973da523be8
javaidb <javaidbaksh@gmail.com>
2024-12-23 02:08:56 -0500

    Added stacked bar plots to visualize amounts + growth visualization of overall balance across all tracked years.

--- src/modules/helper_fns.py
+++ src/modules/helper_fns.py
@@ -1,7 +1,14 @@
 import re
 import calendar
 import os
+import pandas as pd
+
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+from plotly.colors import qualitative
+
+from scipy.ndimage import gaussian_filter1d
 
 def __extract_month(input_string):
     months = '|'.join(f'.*{m}.*' for m in calendar.month_abbr[1:])
@@ -50,13 +57,87 @@ def sort_df(df):
     df = df.sort_values(by='DateTime')
     return df.groupby('DateTime', group_keys=False).apply(lambda x: x.sort_index(), include_groups=True)
 
-def plot_attribute_against_datetime(df, df_col='Balance'):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df['DateTime'], y=df[df_col], mode='markers', name=df_col))
+def plot_attribute_against_datetime(
+        df_in, 
+        df_col='Balance',
+        day_span={
+            'moving_average': 7,
+            'rate_of_change': 7
+        }):
+
+    moving_avg_col = f"moving_average_{day_span['moving_average']}D"
+    rate_of_change_col = f"rate_of_change_{day_span['rate_of_change']}D"
+
+
+    df = df_in.copy()
+    df = df[df[[df_col]].notnull().all(axis=1)]
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
+    df = df.sort_values(by='DateTime')
+    df.set_index('DateTime', inplace=True)
+    df[moving_avg_col] = df[df_col].rolling(window=f"{day_span['moving_average']}D", min_periods=1).mean()
+    df[f"smoothed_{moving_avg_col}"] = gaussian_filter1d(df[moving_avg_col], sigma=10)
+
+    df[rate_of_change_col] = df[f"smoothed_{moving_avg_col}"].rolling(f"{day_span['rate_of_change']}D").apply(lambda x: x.iloc[-1] - x.iloc[0], raw=False)
+    df[rate_of_change_col] = gaussian_filter1d(df[rate_of_change_col], sigma=20)
+    df.reset_index(inplace=True)
+
+    fig = make_subplots(rows=2, cols=1, subplot_titles=('Original Data', f"{day_span['rate_of_change']}-day Rate of Change"),vertical_spacing=0.1,
+                        shared_xaxes=True)
+    fig.add_trace(go.Scatter(x=df['DateTime'], y=df[df_col], mode='markers', name=df_col), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['DateTime'], y=df[f"smoothed_{moving_avg_col}"], mode='lines', name=f"{day_span['moving_average']}-day Moving Avg"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['DateTime'], y=df[rate_of_change_col], mode='lines', name=f"{day_span['rate_of_change']}-day Rate of Change"), row=2, col=1)
+
+    fig.add_trace(go.Scatter(
+        x=df['DateTime'],
+        y=[max(0, y) for y in df[rate_of_change_col]],
+        fill='tozeroy',
+        fillcolor='rgba(0, 255, 0, 0.1)',
+        line=dict(width=0),
+        showlegend=False
+    ), row=2, col=1)
+
+    # Add shaded area for negative values (not in legend)
+    fig.add_trace(go.Scatter(
+        x=df['DateTime'],
+        y=[min(0, y) for y in df[rate_of_change_col]],
+        fill='tozeroy',
+        fillcolor='rgba(255, 0, 0, 0.1)',
+        line=dict(width=0),
+        showlegend=False
+    ), row=2, col=1)
+
     fig.update_layout(
         title=f'{df_col} Over Time',
         xaxis_title='Date',
         yaxis_title=df_col,
+        height=800,
     )
     fig.show()
     return
+
+def plot_stacked(df_in):
+    df = df_in.copy()
+    timeframe = 'Month'
+    df[timeframe] = pd.to_datetime(df['DateTime']).dt.to_period('M').astype(str)
+
+    monthly_expenditure = df.groupby([timeframe, 'Classification'])['Amount'].sum().reset_index()
+    
+    extended_colors = qualitative.Plotly + qualitative.D3
+    fig = px.bar(monthly_expenditure, 
+                x=timeframe, 
+                y='Amount', 
+                color='Classification',
+                color_discrete_sequence=extended_colors,
+                title='Monthly Expenditure by Category',
+                labels={'Amount': 'Total Expenditure', timeframe: timeframe},
+                barmode='stack')
+
+    fig.update_layout(
+        barmode='relative',  # Stacks positive above and negative below the x-axis
+        xaxis_title=timeframe,
+        yaxis_title='Total Expenditure',
+        legend_title='Classification',
+        bargap=0.1
+    )
+
+    fig.show()
