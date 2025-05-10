2dd693009fcbfdd889ed7fb08ac39b1f5759376c
Javaid Baksh <javaidbaksh@gmail.com>
2025-05-10 17:49:20 -0400

    Added category route

--- src/web/routes/__init__.py
+++ src/web/routes/__init__.py
@@ -1,7 +1,6 @@
-"""Routes package."""
-
 from .main import main_bp
 from .api import api_bp
 from .merchants import merchants_bp
+from .category_routes import category_bp
 
-__all__ = ['main_bp', 'api_bp', 'merchants_bp'] 
+__all__ = ['main_bp', 'api_bp', 'merchants_bp', 'category_bp']
